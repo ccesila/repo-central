e42d29d873a12c78f7c1be9a1ab7dbc139618ead
Celso H. Cesila <cesila@unicamp.br>
2023-01-03 19:03:16 -0300

    Revert "classe emprestimo"
    This reverts commit eb77bdcf2c534bc4accd68b8cdee120f20c846e8.

--- src/biblioteca/BibliotecApp.java
+++ src/biblioteca/BibliotecApp.java
@@ -4,9 +4,6 @@ public class BibliotecApp {
 
 	public static void main(String[] args) {
 		Livro l = new Livro();
-		Usuario u = new Usuario();
-		
-		Emprestimo emprestimo = new Emprestimo();
 
 	}
 
