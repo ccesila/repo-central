eb77bdcf2c534bc4accd68b8cdee120f20c846e8
Celso H. Cesila <cesila@unicamp.br>
2023-01-03 19:01:14 -0300

    classe emprestimo

--- src/biblioteca/BibliotecApp.java
+++ src/biblioteca/BibliotecApp.java
@@ -4,6 +4,9 @@ public class BibliotecApp {
 
 	public static void main(String[] args) {
 		Livro l = new Livro();
+		Usuario u = new Usuario();
+		
+		Emprestimo emprestimo = new Emprestimo();
 
 	}
 
